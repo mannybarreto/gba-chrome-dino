9fc3e51b3b029ca264d0be0963d386016e1e8e8c
Manny Barreto <ma.barre98@gmail.com>
2024-12-28 15:00:57 -0500

    Fixed floor width calculation to be the number of tiles, and not the number of bits.

--- source/template.c
+++ source/template.c
@@ -51,7 +51,10 @@ void render_character() {
 }
 
 void init_map() {
-  for (int i = 0; i < sizeof(se_mat[SBB][FLOOR_TY]); i++) {
+  // Calculate the width of the screenblock matrix row in tiles.
+  int floor_width_tiles =
+      sizeof(se_mat[SBB][FLOOR_TY]) / sizeof(se_mat[SBB][FLOOR_TY][0]);
+  for (int i = 0; i < floor_width_tiles; i++) {
     se_mat[SBB][FLOOR_TY][i] = 1;
   }
 }
