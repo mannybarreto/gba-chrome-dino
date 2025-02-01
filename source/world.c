c0d55c6ea4a69b31ce383545ae9c172ae00db1c3
Manny Barreto <ma.barre98@gmail.com>
2025-02-01 11:55:44 -0800

    Revert "Moved world set up into world.c"
    This reverts commit 7cf43297b2e0acdffae87a2adb76d2f86b2b36bf.

--- source/world.c
+++ source/world.c
@@ -1,20 +1,7 @@
 #include "world.h"
-
 #include <stdlib.h>
-#include <string.h>
-#include <tonc.h>
-
-#include "background.h"
 
 void init_world(int SBB) {
-  // Setting up the tiles:
-  memcpy(pal_bg_mem, backgroundPal, backgroundPalLen);
-  memcpy(&tile_mem[0][0], backgroundTiles, backgroundTilesLen);
-
-  //   using charblock 0 and screenblock 31
-  REG_BG0CNT = BG_CBB(0) | BG_SBB(SBB) | BG_4BPP | BG_REG_32x32;
-  REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
-  //
   // Calculate the width of the screenblock matrix row in tiles.
   int floor_width_tiles =
       sizeof(se_mat[SBB][FLOOR_TY]) / sizeof(se_mat[SBB][FLOOR_TY][0]);
--- source/world.h
+++ source/world.h
@@ -3,7 +3,7 @@
 #include <tonc.h>
 
 // Tile y in tiles.
-#define FLOOR_TY (SCREEN_HEIGHT_T - 2)
+#define FLOOR_TY (SCREEN_HEIGHT_T - 1)
 
 #define FLOOR_Y (8 * FLOOR_TY)
 
